1f0ab058210b9c0b99b290e3c2e88aa4bf8b0994
Emaglp <galuppin@insa-toulouse.fr>
2025-02-18 11:50:36 +0100

    commentaire sur code florian

--- Code_Florian.py
+++ Code_Florian.py
@@ -22,6 +22,7 @@ class AE(torch.nn.Module):
         super().__init__()
 
         self.n_pixels = n_pixels
+        
         # Building a linear encoder with Linear
         # layer followed by Relu activation function
         self.encoder = torch.nn.Sequential(
@@ -35,6 +36,7 @@ class AE(torch.nn.Module):
             torch.nn.ReLU(),
             torch.nn.Linear(8, 4)
         )
+
 
         # Building an linear decoder with Linear
         # layer followed by Relu activation function
@@ -82,6 +84,7 @@ def train_DL_model(model, n_epochs, loader, loss_function):
     return model, losses
 
 
+
 def binning_spectral(X, nb_band_final):
     # l'étape par reduction de bande permet de reduire la compléxité calculatoire
     # et de se rapprocher de l'hypothèse d'independance des données
@@ -106,6 +109,8 @@ def RX_global(im_bin):
     # mat_centre=np.tile(centre, ( Nbpix,1))
     for i in range(n_pix):
         point = im_bin[i, :]
+        #distance de malanobis pour chaque pixel on compare chacunes de ses longueurs d'ondes avec les longueurs d'ondes moyennes
+        #on utilise l'inverse de la mat cov dans le calcul de malanobis
         distance[i] = mahalanobis(point, centre, inv_matcov)
     return distance
 
@@ -116,11 +121,13 @@ if __name__ == "__main__":
    # os.environ['SPECTRAL_DATA'] = 'C:/Users/Admin/Documents/insa/4A/PIR/Hyperspectral_Anomalies/DATA0_INSA/image1'
     os.environ['SPECTRAL_DATA'] = 'C:/Users/emaga/GIT/PIR/DATA0_INSA/image1'
     pathproj = "scene_lac.hdr"
+    
     # LECTURE IMAGE
     img = spec.open_image(pathproj)
     proj = img.load()
     proj = proj.squeeze()
 
+    #donne des infos caractéristiques de l'img (nb col, nb ligne, nb pixels, nb bandes)
     Nbcol = proj.shape[1]
     Nblig = proj.shape[0]
     Nbpix = Nbcol * Nblig
@@ -128,22 +135,41 @@ if __name__ == "__main__":
 
     n_bin = 30
 
+    #ils mettent le cube en matrice (chaquse lignes representent un pixel avec dans chacunes des col une de ses bandes)
     data_im = np.reshape(proj, [Nbpix, Nbband])
+    
+    #image binaire : 
     im_bin = binning_spectral(data_im, n_bin)
 
+
     # RX method
+    #calcul d'un vecteur distance qui stock toutes les distances pour chaques pixels (1 d pour 1 pix)
     distance = RX_global(im_bin)
+    #reshape en mode matrice avec le bon nombre de ligne et de col 
+    #c'est ce qui va être affiché
     distance_carte = np.reshape(distance, [Nblig, Nbcol])
 
+    #première image que l'on affiche, on l'a recentrée avec data_im.max()
     data_img_matrix = data_im.reshape(Nblig, Nbcol, Nbband) / data_im.max()
     data_img_bin_matrix = im_bin.reshape(Nblig, Nbcol, n_bin)
 
     # we can do some anomaly detection using IF algorithm
+    #Return the anomaly score of each sample using the IsolationForest algorithm
+    #The IsolationForest ‘isolates’ observations by randomly selecting a feature and then randomly selecting a split value between the maximum and minimum values of the selected feature.
+    #Since recursive partitioning can be represented by a tree structure, the number of splittings required to isolate a sample is equivalent to the path length from the root node to the terminating node.
+    #This path length, averaged over a forest of such random trees, is a measure of normality and our decision function.
+    #Random partitioning produces noticeably shorter paths for anomalies. Hence, when a forest of random trees collectively produce shorter path lengths for particular samples, they are highly likely to be anomalies.
+    
+    #n-jos=-1 => utilise tout les coeurs de l'ordi pour une meilleur perf
+    #n-estimators : nombre d'arbres que l'on fait
+    #max_samples: chaque arbre est entrainé qu'à 70% sur les données (permet d'éviter le sur apprentissage)
+   
     IF = IsolationForest(n_jobs=-1, n_estimators=100, max_samples=0.7)
     IF.fit(im_bin)
     IF_scores = IF.score_samples(im_bin)
     im_IF_score = IF_scores.reshape(Nblig, Nbcol)
     # im_IF_score = np.where(im_IF_score < -0.65, 1, 0)
+
 
     # DL training a AE
 
@@ -175,6 +201,8 @@ if __name__ == "__main__":
     diff_AE = diff_AE.reshape(Nblig, Nbcol, n_bin)
     
     diff_AE = np.mean((diff_AE**2),axis=2)
+
+
 
     # plot some figure as exemple
     print("size img", data_im.shape)
